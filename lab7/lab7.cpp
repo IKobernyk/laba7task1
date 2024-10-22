c150af6da1f2b425d4563a3cde53cc78d28259b2
IKobernyk <kobernuk.ira15@gmail.com>
2024-10-22 14:52:14 +0300

    Add files via upload

---
+++ lab7/lab7.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string capital;
+
+    // Введення назви столиці
+    cout << "Enter the name of the capital: ";
+    cin >> capital;
+
+    // Перевірка на відповідність столиці країні
+    if (capital == "Kyiv") {
+        cout << "Ukraine" << endl;
+    }
+    else if (capital == "Washington") {
+        cout << "USA" << endl;
+    }
+    else if (capital == "London") {
+        cout << "United Kingdom" << endl;
+    }
+    else if (capital == "Paris") {
+        cout << "France" << endl;
+    }
+    else if (capital == "Berlin") {
+        cout << "Germany" << endl;
+    }
+    else if (capital == "Tokyo") {
+        cout << "Japan" << endl;
+    }
+    else if (capital == "Moscow") {
+        cout << "Russia" << endl;
+    }
+    else if (capital == "Beijing") {
+        cout << "China" << endl;
+    }
+    else if (capital == "Ottawa") {
+        cout << "Canada" << endl;
+    }
+    else if (capital == "Canberra") {
+        cout << "Australia" << endl;
+    }
+    else {
+        cout << "Capital not found in the database." << endl;
+    }
+
+    return 0;
+}
